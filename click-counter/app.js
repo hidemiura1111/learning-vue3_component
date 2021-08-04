c02d51a6cf2608b5c5d48d2297fc0a24ce567d48
hidems <miura.eleven@gmail.com>
2021-08-04 23:31:03 +0200

    x-template

--- click-counter/app.js
+++ click-counter/app.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
 })
   
 .component('click-counter', {
-  template: '<button @click="count++">{{count}}</button>',
+  // template: '<button @click="count++">{{count}}</button>',
+  template: '#click-counter-template',
   data() {
     return {
       count: 0
