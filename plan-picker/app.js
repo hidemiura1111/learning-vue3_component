10c20cc10fccc7ad256e23d4e80d503ad3350504
hidems <miura.eleven@gmail.com>
2021-08-05 00:16:29 +0200

    plan-picker, Components with props

---
+++ plan-picker/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict']
+    }
+  }
+})
+  
+.component('plan', {
+  template: '#plan-template',
+  props: {
+    name: { type: String, required: true }
+  }
+})
+.mount('#app')
