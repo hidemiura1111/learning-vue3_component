61fad801504ed967a13121aa225dd2b2bf259105
hidems <miura.eleven@gmail.com>
2021-08-08 01:47:18 +0200

    global vs local vue components

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,4 +1,22 @@
+let PlanComponent = {
+  template: '#plan-template',
+  props: {
+    name: { type: String, required: true }
+  }
+}
+
+let PlanPickerComponent = {
+  components: { plan: PlanComponent },
+  template: '#plan-picker-template',
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict']
+    }
+  }
+}
+
 const app = Vue.createApp({
+  components: { 'plan-picker': PlanPickerComponent }
   // data() {
   //   return {
   //     plans: ['The Single', 'The Curious', 'The Addict']
@@ -6,19 +24,20 @@ const app = Vue.createApp({
   // }
 })
   
-.component('plan-picker', {
-  template: '#plan-picker-template',
-  data() {
-    return {
-      plans: ['The Single', 'The Curious', 'The Addict']
-    }
-  }
-})
+// .component('plan-picker', {
+//   components: { plan: PlanComponent },
+//   template: '#plan-picker-template',
+//   data() {
+//     return {
+//       plans: ['The Single', 'The Curious', 'The Addict']
+//     }
+//   }
+// })
   
-.component('plan', {
-  template: '#plan-template',
-  props: {
-    name: { type: String, required: true }
-  }
-})
+// .component('plan', {
+//   template: '#plan-template',
+//   props: {
+//     name: { type: String, required: true }
+//   }
+// })
 .mount('#app')
